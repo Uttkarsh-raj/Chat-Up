b6cf5c4dc1fa82345fe37bf7ae632557dc95a001
Uttkarsh-raj <uttkarsh.raj.jsr@gmail.com>
2023-04-03 23:34:27 +0530

    Home page basic UI

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -1,11 +1,53 @@
+import 'package:chatit/view/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../constants/assets_constants.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold();
+    TextEditingController searchController = TextEditingController();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5, 20, 10, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  CircleAvatar(
+                    backgroundImage: Assets.logo,
+                    radius: 30,
+                  ),
+                  Text(
+                    'Chat-Up',
+                    style: TextStyle(
+                      fontSize: 27,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Icon(
+                    Icons.menu,
+                    size: 26,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 12),
+            CustomTextField(
+              label: 'Search',
+              icon: Icons.search_outlined,
+              obscure: false,
+              controller: searchController,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
